9f19d0edfa525995e1d06e21038a9ffbe31f5fb5
hjonesmtb <holdenjones15@gmail.com>
2022-01-18 13:00:14 -0800

    added system control for pstat

--- system_control.py
+++ system_control.py
@@ -7,9 +7,6 @@ import numpy as np
 
 from syringe_pump.pump_22 import Pump
 
-
-
-
 def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
@@ -19,7 +16,7 @@ def draw_figure(canvas, figure, loc=(0, 0)):
 #boiler plate code for start page. Choose COM ports
 def com_windows():
     layout = [
-			     [sg.Text('Valve Control', size=(40, 1),
+			     [sg.Text('Pump Control', size=(40, 1),
 					justification='center', font='Helvetica 20')],
        			 [sg.Text('Syringe Pump COM', size=(15, 1), font='Helvetica 12'), sg.InputText('12')],
        			 [sg.Text('Syringe Pump Baudrate', size=(15, 1), font='Helvetica 12'), sg.InputText('1200')],
@@ -43,12 +40,26 @@ def com_windows():
 #boiler plate code for entering parameters 
 def control_windows():
     layout = [
-			     [sg.Text('Valve Control', size=(40, 1),
+			     [sg.Text('Pump Control', size=(40, 1),
 					justification='center', font='Helvetica 20')],
 
 				 [sg.Text('Flow rate [uL/min]', size=(15, 1), font='Helvetica 12'), sg.InputText('1000')],
 				 [sg.Text('Infusion volume [mL]', size=(15, 1), font='Helvetica 12'), sg.InputText('1')],
-				 [sg.Text('pulse period [ms]', size=(15, 1), font='Helvetica 12'), sg.InputText('2000')],
+
+			     [sg.Text('SWV Settings', size=(40, 1),
+					justification='center', font='Helvetica 20')],
+
+				 [sg.Text('E condition [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
+				 [sg.Text('t condition [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
+				 [sg.Text('E deposition [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.8')],
+				 [sg.Text('t deposition [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('60')],
+
+				[sg.Text('t equilibration [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
+				[sg.Text('E begin [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('-0.4')],
+				[sg.Text('E stop [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.4')],
+				[sg.Text('E step [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.005')],
+				[sg.Text('Amplitude [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.01')],
+				[sg.Text('Frequency [Hz]', size=(15, 1), font='Helvetica 12'), sg.InputText('7')],
 
 				 [sg.Button('Start', size=(10, 1), pad=((280, 0), 3), font='Helvetica 14')],
 
@@ -71,11 +82,6 @@ def control_windows():
     
     return window, ax, fig_agg
 
-# TODO: implement this method so that all the data from a single DPV sweep is stored somewhere (CSV file probably)
-def read_pstat():
-	time.sleep(1)
-	return [np.linspace(-0.4,0.4,100), np.random.rand(100,1)]
-
 def main():
 
 	COM_select = com_windows()
@@ -88,48 +94,56 @@ def main():
 			pstat_com = int(values[2])
 			break
 
-	pump = Pump(pump_com, pump_baud)
-
 	# TODO: connect to pstat using pstat_com 
 
 	COM_select.close()
 
 	window, ax, fig_agg = control_windows()
-	flow_rate, volume, period = 0,0,0
+
+	# Measurement parameters
+	flow_rate, volume = 0,0
+	e_cond, e_dep, e_begin, e_end, e_step = 0,0,0,0,0
+	t_cond, t_dep, t_equil = 0,0,0
+	amplitude, frequency = 0, 0
+
 	# Enter measurement parameters and start pumping
 	while True:
 		event, values = window.read(timeout=10)
 
 		if event in ('Start', None):
 
-			flow_rate = int(values[0])
-			volume = int(values[1])
-			period = int(values[2])
+			flow_rate, volume = int(values[0]), int(values[1])
 
-			pump.set_diameter(10) # Fixed syringe diameter
-			pump.set_rate(flow_rate,'uL/min')
-			pump.set_volume(volume)
-
-			pump.reset_acc() # reset accumulated volume to zero
-
+			e_cond, t_cond = float(values[2]), float(values[3])
+			e_dep, t_dep = float(values[4]), int(values[5])
+			t_equil = float(values[6])
+			e_begin, e_end, e_step = float(values[7]), float(values[8]), float(values[9])
+			amplitude, frequency = float(values[10]), float(values[11])
 			break
 
-	pump_on = True  #True when pumping, False when stopped
-	counter = 0
+	# connect to pump
+	pump = Pump(pump_com, pump_baud)
+
+	pump.set_diameter(10) # Fixed syringe diameter
+	pump.set_rate(flow_rate,'uL/min')
+	pump.set_volume(volume)
+	pump.reset_acc() # reset accumulated volume to zero
+
+	# connect to emstat; the parameters could be a list or dictionary
+	pstat = Emstat(pstat_com, e_cond, t_cond, e_dep, t_dep, t_equil, e_begin, e_end, e_step, amplitude, frequency)
 
 	IV = [np.zeros(100), np.zeros(100)]
 
-	# toggle flow on/off at a fixed period
+	# toggle flow on/off while measuring pstat
 	while True:
 
-		if pump_on:
-			pump.infuse()	# Run flow for 5s between DPV sweeps. Fentanyl --> Norfentanyl
-			window.read(period)
-		else:
-			pump.stop() 	# While flow is stopped, store pstat data in csv 
-			IV = read_pstat()  
+		# start flow, deposit norfentynal 
+		pump.infuse()
+		pstat.deposition() # this takes ~10-20 secs, during which GUI is bricked
 
-		pump_on = not pump_on
+		# stop flow, run SWV sweep
+		pump.stop()
+		IV = pstat.sweepSWV() # this takes ~10-20 secs, during which GUI is bricked
 
 		ax.grid() # draw the grid
 		ax.plot(IV[0],IV[1]) #plot new pstat readings
