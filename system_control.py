f05e089959e4b8e50fa719decff7e1ae0a8b99ac
mwesteinde <54851917+mwesteinde@users.noreply.github.com>
2022-01-29 10:52:35 -0800

    Deposition time now controlled by the flow rate and the deposition step volume

--- system_control.py
+++ system_control.py
@@ -42,34 +42,35 @@ def com_windows():
 #boiler plate code for entering parameters
 def control_windows():
     layout = [
-			     [sg.Text('Pump Control', size=(40, 1),
-					justification='center', font='Helvetica 20')],
+            [sg.Text('Pump Control', size=(40, 1),
+            justification='center', font='Helvetica 20')],
 
-				 [sg.Text('Flow rate [uL/min]', size=(15, 1), font='Helvetica 12'), sg.InputText('1000')],
-				 [sg.Text('Infusion volume [mL]', size=(15, 1), font='Helvetica 12'), sg.InputText('1')],
+            [sg.Text('Flow rate [uL/min]', size=(15, 1), font='Helvetica 12'), sg.InputText('1000')],
+            [sg.Text('Infusion volume [mL]', size=(15, 1), font='Helvetica 12'), sg.InputText('1')],
+            [sg.Text('Volume/flush [mL]', size=(15, 1), font='Helvetica 12'), sg.InputText('1')],
 
-			     [sg.Text('SWV Settings', size=(40, 1),
-					justification='center', font='Helvetica 20')],
+            [sg.Text('SWV Settings', size=(40, 1),
+            justification='center', font='Helvetica 20')],
 
-				 [sg.Text('E condition [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
-				 [sg.Text('t condition [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
-				 [sg.Text('E deposition [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.8')],
-				 [sg.Text('t deposition [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('60')],
+            [sg.Text('E condition [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
+            [sg.Text('t condition [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
+            [sg.Text('E deposition [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.8')],
+            # [sg.Text('t deposition [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('5')], #Deposition time depends on flowrate and volume/flush
 
-				[sg.Text('t equilibration [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
-				[sg.Text('E begin [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('-0.4')],
-				[sg.Text('E stop [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.4')],
-				[sg.Text('E step [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.005')],
-				[sg.Text('Amplitude [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.01')],
-				[sg.Text('Frequency [Hz]', size=(15, 1), font='Helvetica 12'), sg.InputText('7')],
+            [sg.Text('t equilibration [s]', size=(15, 1), font='Helvetica 12'), sg.InputText('0')],
+            [sg.Text('E begin [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('-0.4')],
+            [sg.Text('E stop [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.4')],
+            [sg.Text('E step [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.005')],
+            [sg.Text('Amplitude [V]', size=(15, 1), font='Helvetica 12'), sg.InputText('0.01')],
+            [sg.Text('Frequency [Hz]', size=(15, 1), font='Helvetica 12'), sg.InputText('7')],
 
-				 [sg.Button('Start', size=(10, 1), pad=((280, 0), 3), font='Helvetica 14')],
+            [sg.Button('Start', size=(10, 1), pad=((280, 0), 3), font='Helvetica 14')],
 
-		         [sg.Canvas(key='controls_cv')],
-                 [sg.Canvas(size=(650, 30), key='-CANVAS-')],
+            [sg.Canvas(key='controls_cv')],
+            [sg.Canvas(size=(650, 30), key='-CANVAS-')],
 
-		        [sg.Button('Exit', size=(10, 1), pad=((280, 0), 3), font='Helvetica 14')]
-		      ]
+            [sg.Button('Exit', size=(10, 1), pad=((280, 0), 3), font='Helvetica 14')]
+            ]
 
     window = sg.Window('Start Screen',
                 layout, finalize=True, resizable=True)
@@ -111,11 +112,11 @@ def main():
         event, values = window.read(timeout=10)
 
         if event in ('Start', None):
-            flow_rate, volume = int(values[0]), int(values[1])
+            flow_rate, volume, step_volume = int(values[0]), int(values[1]), int(values[2])
 
 
-            e_cond, t_cond = float(values[2]), float(values[3])
-            e_dep, t_dep = float(values[4]), int(values[5])
+            e_cond, t_cond = float(values[3]), float(values[4])
+            e_dep = float(values[5])
             t_equil = float(values[6])
             e_begin, e_end, e_step = float(values[7]), float(values[8]), float(values[9])
             amplitude, frequency = float(values[10]), float(values[11])
@@ -133,6 +134,8 @@ def main():
     pstat = Emstat(pstat_com, e_cond, t_cond, e_dep, t_dep, t_equil, e_begin, e_end, e_step, amplitude, frequency)
 
     IV = [np.zeros(100), np.zeros(100)]
+
+    t_dep = step_volume / (flow_rate / 1000 / 60)
 
 	# toggle flow on/off while measuring pstat
 
